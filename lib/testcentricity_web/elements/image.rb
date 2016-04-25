d121764932bf896897fc1fd23120c18a627c2ffc
Tony Mrozinski <tony.mrozinski@dat.com>
2016-04-25 11:14:37 -0700

    Refactored mobile device data to use devices.yml file

--- lib/testcentricity_web/elements/image.rb
+++ lib/testcentricity_web/elements/image.rb
@@ -19,19 +19,19 @@ module TestCentricity
       object_not_found_exception(obj, nil)
       obj.native.attribute('complete')
     end
-  end
 
-  # Wait until the image is fully loaded, or until the specified wait time has expired.
-  #
-  # @param seconds [Integer or Float] wait time in seconds
-  # @example
-  #   company_logo_image.wait_until_loaded(5)
-  #
-  def wait_until_loaded(seconds = nil)
-    timeout = seconds.nil? ? Capybara.default_max_wait_time : seconds
-    wait = Selenium::WebDriver::Wait.new(timeout: timeout)
-    wait.until { is_loaded? }
-  rescue
-    raise "Image #{@locator} failed to load within #{timeout} seconds" unless is_loaded?
+    # Wait until the image is fully loaded, or until the specified wait time has expired.
+    #
+    # @param seconds [Integer or Float] wait time in seconds
+    # @example
+    #   company_logo_image.wait_until_loaded(5)
+    #
+    def wait_until_loaded(seconds = nil)
+      timeout = seconds.nil? ? Capybara.default_max_wait_time : seconds
+      wait = Selenium::WebDriver::Wait.new(timeout: timeout)
+      wait.until { is_loaded? }
+    rescue
+      raise "Image #{@locator} failed to load within #{timeout} seconds" unless is_loaded?
+    end
   end
 end
