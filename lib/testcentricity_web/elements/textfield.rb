45ef9187fe0afc9602508c502e4a6014d4a02cad
Tony Mrozinski <tony.mrozinski@dat.com>
2016-05-02 08:16:28 -0700

    Refactored TextField class definition

--- lib/testcentricity_web/elements/textfield.rb
+++ lib/testcentricity_web/elements/textfield.rb
@@ -7,41 +7,41 @@ module TestCentricity
       @type    = :textfield
       @alt_locator = nil
     end
-  end
 
-  # Is text field set to read-only?
-  #
-  # @return [Boolean]
-  # @example
-  #   comments_field.read_only?
-  #
-  def read_only?
-    obj, _ = find_element
-    object_not_found_exception(obj, nil)
-    !!obj.native.attribute('readonly')
-  end
+    # Is text field set to read-only?
+    #
+    # @return [Boolean]
+    # @example
+    #   comments_field.read_only?
+    #
+    def read_only?
+      obj, _ = find_element
+      object_not_found_exception(obj, nil)
+      !!obj.native.attribute('readonly')
+    end
 
-  # Return maxlength character count of a text field.
-  #
-  # @return [Integer]
-  # @example
-  #   max_num_chars = comments_field.get_max_length
-  #
-  def get_max_length
-    obj, _ = find_element
-    object_not_found_exception(obj, nil)
-    obj.native.attribute('maxlength')
-  end
+    # Return maxlength character count of a text field.
+    #
+    # @return [Integer]
+    # @example
+    #   max_num_chars = comments_field.get_max_length
+    #
+    def get_max_length
+      obj, _ = find_element
+      object_not_found_exception(obj, nil)
+      obj.native.attribute('maxlength')
+    end
 
-  # Return placeholder text of a text field.
-  #
-  # @return [String]
-  # @example
-  #   placeholder_message = username_field.get_placeholder
-  #
-  def get_placeholder
-    obj, _ = find_element
-    object_not_found_exception(obj, nil)
-    obj.native.attribute('placeholder')
+    # Return placeholder text of a text field.
+    #
+    # @return [String]
+    # @example
+    #   placeholder_message = username_field.get_placeholder
+    #
+    def get_placeholder
+      obj, _ = find_element
+      object_not_found_exception(obj, nil)
+      obj.native.attribute('placeholder')
+    end
   end
 end
