4426caa5b0b23317bae72e1ac11efcfb91e9d886
Tony Mrozinski <tony.mrozinski@dat.com>
2016-07-27 14:30:19 -0700

    Added support for parallel test execution in data sourcing

--- lib/testcentricity_web/data_objects_helper.rb
+++ lib/testcentricity_web/data_objects_helper.rb
@@ -44,19 +44,21 @@ module TestCentricity
 
 
   class ExcelDataSource < TestCentricity::DataSource
-    def pick_excel_data_source(sheet, row_name)
+    def pick_excel_data_source(sheet, row_spec)
       environment = ENV['TEST_ENVIRONMENT']
       data_file = "#{XL_PRIMARY_DATA_PATH}#{environment}_data.xls"
-      data_file = XL_PRIMARY_DATA_FILE unless ExcelData.rowspec_exists?(data_file, sheet, row_name)
+      data_file = XL_PRIMARY_DATA_FILE unless ExcelData.rowspec_exists?(data_file, sheet, row_spec)
       data_file
     end
 
-    def read_excel_row_data(sheet, row_name)
-      ExcelData.read_row_data(pick_excel_data_source(sheet, row_name), sheet, row_name)
+    def read_excel_row_data(sheet, row_name, parallel = false)
+      (parallel == :parallel && ENV['PARALLEL']) ? row_spec = "#{row_name}#{ENV['TEST_ENV_NUMBER']}" : row_spec = row_name
+      ExcelData.read_row_data(pick_excel_data_source(sheet, row_spec), sheet, row_spec)
     end
 
-    def read_excel_pool_data(sheet, row_name)
-      ExcelData.read_row_from_pool(pick_excel_data_source(sheet, row_name), sheet, row_name)
+    def read_excel_pool_data(sheet, row_name, parallel = false)
+      (parallel == :parallel && ENV['PARALLEL']) ? row_spec = "#{row_name}#{ENV['TEST_ENV_NUMBER']}" : row_spec = row_name
+      ExcelData.read_row_from_pool(pick_excel_data_source(sheet, row_name), sheet, row_spec)
     end
 
     def read_excel_range_data(sheet, range_name)
